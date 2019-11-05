af08178aba02ed6e6394468649fe83fc843e20f2
2nc <wnchit@163.com>
2019-11-05 23:53:25 +0800

    can start userapp when start system

--- app/config.py
+++ app/config.py
@@ -1,13 +1,16 @@
-ami_id = 'ami-0df7fa91aa9ef7ed9' #need change to new ami
+ami_id = 'ami-0df7fa91aa9ef7ed9'  # need change to new ami
 EC2_count = 1
-EC2_userdata = 'userdata' #casual
+EC2_userdata = '''#!/bin/bash
+python3 -m pip install --user boto3
+sleep 20
+cd /home/ubuntu/Desktop/A1_3.0 
+python3 ./run.py >& /tmp/message'''
 EC2_instance = 't2.micro'
-EC2_keyName = 'A1' #use your config
-EC2_security_group_id = ['sg-eb879abb'] #use your config
+EC2_keyName = 'A1'  # use your config
+EC2_security_group_id = ['sg-eb879abb']  # use your config
 EC2_monitor = True
-EC2_target_key = 'worker' #value in tag casual
-EC2_target_value = 'work' #value in tag casual
-
+EC2_target_key = 'worker'  # value in tag casual
+EC2_target_value = 'work'  # value in tag casual
 
 db_config = {'user': 'admin',
              'password': '12345678',
@@ -15,5 +18,5 @@ db_config = {'user': 'admin',
              'database': 'mydb'}
 
 DATABASE_ID = 'alldata'
-MANAGER_ID = 'i-04ec52fb3814d9025' #change
+MANAGER_ID = 'i-04ec52fb3814d9025'  # change
 S3_BUCKET_NAME = 'a2homework'
